b31b5f9c6a22a2c2dd9d275fe74dcf4236c3076d
solomon244 <slegesse2444@gmail.com>
2022-05-31 14:09:58 -0700

    reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-c = 0
+
+
+nc = 0
 for n in range(122, 96, -1):
     if n % 2 == 0:
-         c = n
+        nc = n
     else:
-         c = n - 32
-    print("{}".format(chr(c)), end='')
+        nc = n - 32
+    print("{}".format(chr(nc)), end='')
