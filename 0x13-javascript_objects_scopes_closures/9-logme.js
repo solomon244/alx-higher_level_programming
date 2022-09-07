4dc79dacf05661c61bdd2c8531e5c71f7dda2d26
solomon244 <slegesse2444@gmail.com>
2022-09-08 02:54:18 +0300

    function that prints the number of arguments already printed

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -5,27 +5,3 @@ exports.logMe = function (item) {
   console.log(`${i}: ${item}`);
   i += 1;
 };
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-9-logme.js [unix] (02:59 01/01/1970)                                     0,1 All
--- INSERT --
-
