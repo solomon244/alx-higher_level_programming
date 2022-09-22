4e838738ff4660d4f40f2eb4ed459e8bd91286bd
solomon legesse <slegesse2444@gmail.com>
2022-09-22 23:51:17 +0300

    takes in a URL, sends a GET request URL, and displays the body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
+curl -sLX Get "$1" 
 
